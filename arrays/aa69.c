f5e132adc5a664b79d51df44fcadd31454abdba2
DhruvJha707 <dhruvjha707@gmail.com>
2026-02-17 21:25:16 +0530

    Initial commit

---
+++ arrays/aa69.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("You entered: ");
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
